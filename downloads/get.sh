8995ff20c7c12d864426180aa3810a8d5efe807b
Duane Johnson <duane.johnson@gmail.com>
2016-04-06 10:52:53 -0600

    Add get.sh script for anaconda, cuda

---
+++ downloads/get.sh
@@ -0,0 +1,4 @@
+wget http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.4.1-Linux-x86_64.sh
+# wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-4.0.0-Linux-x86_64.sh
+
